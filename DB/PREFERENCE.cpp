8434d27df7bd756f50103ee567c566ac7058668b
CHEN ChengYi <63190590+1155126781@users.noreply.github.com>
2021-03-19 16:47:05 +0800

    preference DB
    preference DB and a global variable dictionary(I think maybe we need to communicate later about what global variables we should use)

---
+++ DB/PREFERENCE.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <module1.h>
+using namespace std;
+
+int main() {
+
+for (int i = 0; i < 1000; i++){
+       cin >> usernameList[i]
+       for (int j = 0; j<12; j++){
+           if (userPreference[j] == "photography"){
+               photography[k] = usernameList[i];
+               photographynum++;
+           }
+           else if(userPreference[j] == "singing"){
+               singing[k] = usernameList[i];
+               singingnum++; 
+           }
+           else if(userPreference[j] == "music"){
+               music[k] = usernameList[i];
+               musicnum++; 
+           }
+           else if(userPreference[j] == "music"){
+               music[k] = usernameList[i];
+               musicnum++; 
+           }
+           else if(userPreference[j] == "gym"){
+               gym[k] = usernameList[i];
+               gymnum++; 
+           }
+           else if(userPreference[j] == "arts"){
+               arts[k] = usernameList[i];
+               artsnum++; 
+           }
+           else if(userPreference[j] == "sports"){
+               sports[k] = usernameList[i];
+               sportsnum++; 
+           }
+           else if(userPreference[j] == "hiking"){
+               hiking[k] = usernameList[i];
+               hikingnum++; 
+           }
+           else if(userPreference[j] == "reading"){
+               reading[k] = usernameList[i];
+               readingnum++; 
+           }
+           else if(userPreference[j] == "coffee"){
+               coffee[k] = usernameList[i];
+               coffeenum++;
+           }
+           else if(userPreference[j] == "gaming"){
+               gaming[k] = usernameList[i];
+               gamingnum++; 
+           }
+           else if(userPreference[j] == "Korean Drama"){
+               koreanDrama[k] = usernameList[i];
+               koreanDramanum++; 
+           }
+           else if(userPreference[j] == "engineering"){
+               engineering[k] = usernameList[i];
+               engineerinnum++; 
+           }
+           else
+           return;
+        }
+        return;
+   }
+return;
+}
---
+++ DB/module1.h
@@ -0,0 +1,31 @@
+#ifndef MODULE_1_H
+#define MODULE_1_H
+
+extern string usernameList[1000];
+extern string userPreference[11]; //每個人個userPreference係佢地揀既preference,所以有好多combinations
+extern string photography[100];
+extern int photographynum = 0;
+extern string singing[100];
+extern int singingnum = 0;
+extern string music[100];
+extern int musicnum = 0;
+extern string gym[100];
+extern int gymnum = 0;
+extern string arts[100];
+extern int artsnum = 0;
+extern string sports[100];
+extern int sportsynum = 0;
+extern string hiking[100];
+extern int hikingnum = 0;
+extern string reading[100];
+extern int readingnum = 0;
+extern string coffee[100];
+extern int coffeenum = 0;
+extern string gaming[100];
+extern int gamingnum = 0;
+extern string koreanDrama[100];
+extern int koreanDramanum = 0;
+extern string engineering[100];
+extern int engineerinnum = 0;
+
+#endif
